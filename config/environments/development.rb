e60a42aacfa449fd1140f5cd369fd8886a570156
nolyoi <nolan@moonship.cc>
2020-06-12 23:20:09 -0500

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.web_console.automount = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -51,4 +52,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+end
+
+class Application < Rails::Application
+  config.web_console.whitelisted_ips = '127.0.0.1'
 end
